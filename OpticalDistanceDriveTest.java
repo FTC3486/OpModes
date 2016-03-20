aacc4ee60e2c5b1020142575ddadae433833792d
mdttrump97 <mdttrump97@bellsouth.net>
2016-03-19 19:34:40 -0500

    Adds test program for the Optical Distance Driver

--- OpticalDistanceDriveTest.java
+++ OpticalDistanceDriveTest.java
@@ -1,7 +1,94 @@
+/* Copyright (c) 2015 Qualcomm Technologies Inc
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
 package com.FTC3486.OpModes;
 
+import com.FTC3486.FTCRC_Extensions.AutoDriver;
+import com.FTC3486.FTCRC_Extensions.EncoderAutoDriver;
+import com.FTC3486.FTCRC_Extensions.GyroscopeAutoDriver;
+import com.FTC3486.FTCRC_Extensions.DriveTrain;
+import com.FTC3486.FTCRC_Extensions.ExtendedDcMotor;
+import com.FTC3486.FTCRC_Extensions.OpticalAutoDriver;
+import com.FTC3486.Subsystems.ClimberDump;
+import com.FTC3486.Subsystems.ParkingBrake;
+import com.FTC3486.Subsystems.Pickup;
+import com.FTC3486.Subsystems.Plow;
+import com.FTC3486.Subsystems.TapeMeasure;
+import com.FTC3486.Subsystems.Turret;
+import com.FTC3486.Subsystems.Winch;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
 /**
- * Created by Matthew on 3/19/2016.
+ * Created by Matthew on 11/25/2015.
  */
-public class OpticalDistanceDriveTest {
+public class OpticalDistanceDriveTest extends LinearOpMode {
+    TapeMeasure tapeMeasure;
+    Winch winch;
+    ParkingBrake parkingBrake;
+    Turret turret;
+    Plow plow;
+    Pickup pickup;
+    ClimberDump climberDump;
+    DriveTrain driveTrain;
+    AutoDriver linearDriver;
+    AutoDriver angularDriver;
+    OpticalDistanceSensor opticalDistanceSensor;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor rightback = hardwareMap.dcMotor.get("rightback");
+        rightback.setDirection(DcMotor.Direction.REVERSE);
+        DcMotor rightfront = new ExtendedDcMotor(hardwareMap.dcMotor.get("rightfront"), this);
+        rightfront.setDirection(DcMotor.Direction.REVERSE);
+
+        driveTrain = new DriveTrain.Builder()
+                .addLeftMotor(hardwareMap.dcMotor.get("leftback"))
+                .addLeftMotorWithEncoder(new ExtendedDcMotor(hardwareMap.dcMotor.get("leftfront"), this))
+                .addRightMotor(rightback)
+                .addRightMotorWithEncoder(rightfront)
+                .build();
+        linearDriver = new OpticalAutoDriver(this, driveTrain, "opticalDistanceSensor", hardwareMap);
+        angularDriver = new EncoderAutoDriver(this, driveTrain);
+        tapeMeasure = new TapeMeasure("tapeMotor", "tapeTilt", hardwareMap);
+        winch = new Winch("winchMotor", hardwareMap);
+        parkingBrake = new ParkingBrake("parkingBrake", hardwareMap);
+        turret = new Turret("swivel", "extender", "dumper", "dumperSwivel", hardwareMap);
+        plow = new Plow("leftPlow", "rightPlow", hardwareMap);
+        pickup = new Pickup("pickup", hardwareMap);
+        climberDump = new ClimberDump("climberDump", hardwareMap);
+        opticalDistanceSensor = hardwareMap.opticalDistanceSensor.get("opticalDistanceSensor");
+
+        // wait for the start button to be pressed
+        waitOneFullHardwareCycle();
+
+        waitForStart();
+
+        linearDriver.set_power(0.25);
+        linearDriver.drive_backward(0);
+    }
 }
